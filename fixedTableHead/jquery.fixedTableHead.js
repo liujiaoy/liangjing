26ca88029e5562a89334685ccc1891794b7d716c
LJY <958479953@qq.com>
2020-09-15 17:50:00 +0800

    too long to remember what I push

---
+++ fixedTableHead/jquery.fixedTableHead.js
@@ -0,0 +1,249 @@
+﻿/*
+    实现功能：1.固定搜索栏$("#test").fillHead();  其实就是表格上面的那一部分，不一定非的是搜索栏
+
+              2.固定表头$("#test2").fixedTableHead(height); table必须要有ID!!!(不然ie8那个特立独行的小伙子会出问题)
+              height: 新表头所在位置，不传参数默认在原有位置上显示，为0时在页面顶部显示
+              适用于整个页面滚动
+
+*/
+/* 
+    使用示例：
+    <script src="../js/jquery.fixedTableHead.js"></script>
+    <%--固定搜索栏--%>
+    <div id="fixedHead">
+        我是需要被固定的搜索栏
+    </div>
+    <div>
+        <table id="gvList">
+        <tr>
+            <th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th>
+        </tr>
+        </table>
+    </div>
+    <script type="text/javascript">
+        //固定搜索栏与表头，只在页面进来的时候执行一次
+        $(window).load(function () {
+            $("#fixedHead").fillHead();
+            $("#gvList").fixedTableHead();
+        })
+    </script> 
+
+*/
+; (function ($) {
+    jQuery.fn.extend({
+        //固定表头、参数：newheight  新表头距离窗口顶部的距离，为零时在窗口顶部，不设时为原有位置
+        "fixedTableHead": function (type,newheight) {
+            if ($(this).length > 0) {
+                //消除固定表头对日历控件的影响
+                $("#meizzCalendarLayer").css("position", "fixed");
+
+                $(".auth_keybord").css({
+                    "position": "fixed",
+                    "z-index": "1000"
+                });
+
+                $(window).off("scroll.winScroll");
+                $(window).off("scroll.divScroll");
+                $(window).off("resize.winResize");
+                //消除固定表头对日历控件的影响
+                $("#meizzCalendarLayer").css("position", "fixed");
+                $("#newTableId").remove();
+                //$(this)表调用方法的元素
+                var oldTableId = $(this);
+                var scrollTimer;
+                var oldheadertop = 0;
+                var oldheaderleft = 0;
+                var oldheaderwidth = 0;
+                if (oldTableId.length > 0) {
+                    oldheadertop = oldTableId.offset().top;
+                    oldheaderleft = oldTableId.offset().left;
+                    oldheaderwidth = oldTableId.outerWidth();
+                }
+                // 动态新增表头并复制宽度
+                var newtable = oldTableId.clone(true);
+                newtable.attr("id", "newTableId");
+                // 有thead
+                if (oldTableId.children("thead").length > 0) {
+                    // 添加在原有表格的后面
+                    oldTableId.parent().append(newtable.html(newtable.children("thead")));
+                   
+                } else {
+
+                    // 无thead，原理同上
+                    oldTableId.parent().append(newtable.html(newtable.find("tr").eq(0)))
+                    
+                }
+                // 新表头的位置，默认不显示
+                $("#newTableId").css({
+                    "position": "fixed",
+                    "left": oldheaderleft,
+                    "width": oldheaderwidth,
+                    "display": "none",
+                    "z-index": 100
+                })
+                //确定新表头的高度
+                newheight = ResizeHeight(oldTableId, newheight);
+
+                //窗口大小改变时，对应表头宽度进行自适应
+                $(window).on("resize.winResize", function winResize() {
+                    // 重新计算表头宽高与位置
+                    handleResize(oldTableId, newheight);
+
+                });
+
+                // 当表头超出窗口宽度时，出现滚动条，新加的表头需要跟随滚动条移动
+                //这里有个问题，能不能使页面中的表格固定表头，这样的话就要监听表格的父div或者其他有滚动条的父div，感觉很不清晰的样子
+                if(type ==="divScroll"){
+                    //外层滚动
+                    oldTableId.parent().on("scroll.divScroll", function divScroll() {
+                        //$(window).off("scroll", handleScroll)
+                        // scroll优化
+                      
+                        clearTimeout(scrollTimer);
+                        scrollTimer = setTimeout(function () {
+                            newheight  = oldTableId.parent().offset().top;
+                            handleResize(oldTableId, newheight)
+                            //当表头的位置超过新表头显示的位置，新表头出现
+                            //oldTableId.offset().top 老位置 ，newheight 新位置 ，$(window).scrollTop()滚动高度（递增）
+                            var dist = oldTableId.parent().scrollTop();
+                            if (dist > 0) {
+                                $("#newTableId").fadeIn();
+                            } else {
+                                $("#newTableId").hide();
+                            }
+                        }, 250);
+
+                        // 原表头距离左侧的宽度
+                        var w = oldTableId.offset().left;
+                        // 横向滚动条滚动的距离
+                        var c = $(this).scrollLeft();
+                        // 横向滚动处理
+                        $("#newTableId").css({
+                            "left": -c + w
+                        })
+                    })
+                }else{
+                    $(window).on("scroll.winScroll", function winScroll() {
+                        //$(window).off("scroll", handleScroll)
+                        // scroll优化
+                        clearTimeout(scrollTimer);
+                        scrollTimer = setTimeout(function () {
+                            handleResize(oldTableId, newheight)
+                            //当表头的位置超过新表头显示的位置，新表头出现
+                            //oldTableId.offset().top 老位置 ，newheight 新位置 ，$(window).scrollTop()滚动高度（递增）
+                            var newheight2;
+                            if (newheight == undefined) { newheight2 = oldTableId.offset().top } else { newheight2 = newheight }
+                            var dist = oldTableId.offset().top - newheight2 - $(window).scrollTop();
+                            if (dist < 0) {
+                                $("#newTableId").fadeIn();
+                            } else {
+                                $("#newTableId").hide();
+                            }
+                        }, 250);
+
+                        // 原表头距离左侧的宽度
+                        var w = oldTableId.offset().left;
+                        // 横向滚动条滚动的距离
+                        var c = $(this).scrollLeft();
+                        // 横向滚动处理
+                        $("#newTableId").css({
+                            "left": -c + w
+                        })
+                    })
+                }
+            }
+            
+        },
+        // 固定搜索栏，实现原理：将搜索栏变为fixed，然后添加bcg占位
+        "fillHead": function () {
+            var scrollTimerSearch;
+            $(window).off("resize.winResizeSearch");
+            $(window).off("scroll.winScrollSearch");
+            $(".fillHeadbcg").remove();
+            var fillHead = $(this);
+            // 搜索栏样式改变
+            fillHead.css({
+                "position": "fixed",
+                "top": 0,
+                "width": "100%",
+                "background-color": "#fff",
+                "z-index": 101
+            });
+            // 搜索栏变为fixed之后bcg替它占位
+            fillHead.before('<div class="fillHeadbcg"></div>');
+            $(".fillHeadbcg").height(fillHead.innerHeight());
+            $(".fillHeadbcg").css({
+                "position": "relative",
+                "top": 0,
+                "left": 0,
+                "width": "100%",
+                "background-color": "#fff"
+            })
+            // 窗口大小改变时，对应固定栏高度进行自适应
+            $(window).on("resize.winResizeSearch", function winResizeSearch() {
+                $(".fillHeadbcg").height(fillHead.innerHeight());
+            });
+            // 窗口滚动时，对应固定栏高度进行自适应
+            $(window).on("scroll.winScrollSearch", function winScrollSearch() {
+                clearTimeout(scrollTimerSearch);
+                scrollTimerSearch = setTimeout(function () {
+                    $(".fillHeadbcg").height(fillHead.innerHeight());
+                }, 200)
+            });
+        }
+    });
+})(jQuery);
+
+// 处理窗口大小改变
+function handleResize(oldTableId, newheight) {
+    ResizeHeight(oldTableId, newheight);
+    $("#newTableId").width(oldTableId.outerWidth());
+    if (oldTableId.children("thead").length > 0) {
+        var newTr = $("#newTableId").find('tr');
+        for (var i = 0; i < newTr.length; i++) {
+            var element = newTr.eq(i)
+            element.css('height', oldTableId.find('tr').eq(i).height());
+            for (var j = 0; j < element.find("th").length; j++) {
+                var elementTh = element.find("th").eq(j);
+                elementTh.width(oldTableId.find('tr').eq(i).find('th:eq(' + j + ')').width())
+            }
+        }
+    } else {
+        var element = $("#newTableId").find('tr').eq(0)
+        element.css('height', oldTableId.find('tr').eq(0).height());
+        for (var j = 0; j < element.find("th").length; j++) {
+            var elementTh = element.find("th").eq(j);
+            elementTh.width(oldTableId.find('tr').eq(0).find('th:eq(' + j + ')').width())
+        }
+    }
+}
+//新表头高度
+function ResizeHeight(oldTableId, newheight) {
+    if (newheight == undefined) {
+        $("#newTableId").css({
+            "top": oldTableId.offset().top
+        })
+    } else {
+        $("#newTableId").css({
+            "top": newheight
+        })
+    }
+
+    return newheight;
+
+
+}
+
+
+function ieBrowser() {
+    var browser = navigator.appName;
+    var version = navigator.appVersion.split(";")[1].replace(/[ ]/g,"");
+    if(browser == "Microsoft Internet Explorer" && version == "MSIE6.0") { 
+        return "6";
+    } else if(browser == "Microsoft Internet Explorer" && version == "MSIE7.0") { 
+        return "7";
+    } else if(browser == "Microsoft Internet Explorer" && version == "MSIE8.0") { 
+        return "8";
+    }
+}
+
--- zongjie/code/freezeTableHeader/fixedTableHead.js
+++ zongjie/code/freezeTableHeader/fixedTableHead.js
@@ -1,179 +1,188 @@
 /*
-    说明： 1.适用于单行或多行表头，固定thead，若无thead，默认固定table第一行（需要有th）
-           2.fixedTableHead适用于整个窗口滚动
-           3.fixedTableHead2适用于嵌套在页面中的表格，表格滚动，表格外层需包裹div
+    实现功能：1.固定搜索栏$("#test").fillHead();
 
-    使用： 在需要使用的页面添加此文件
-            将需要固定表头的tableId传入该方法
-            例 fixedTableHead("#test")
+              2.固定表头$("#test2").fixedTableHead(height);
+              height: 新表头所在位置，不传参数默认在原有位置上显示，为0时在页面顶部显示
+              适用于整个页面滚动
+
 */ 
-{/* 
-    
-<script type="text/javascript" src="../js/fixedTableHead.js"></script>
-<%--固定搜索栏--%>
-<div class="fixedHead"></div>
-<script type="text/javascript">
-    //固定搜索栏与表头
-    $(document).ready(function () {
-        $("#newTableId").remove();
-        fillHead(".fixedHead");
-        fixedTableHead("#gvList");
-    })
-</script> 
-
-*/}
-function fixedTableHead(id){
-    // console.log(id)
-    var oldTableId=$(id);
-    var h=oldTableId.offset().top;
-    var w=oldTableId.offset().left;
-    var d = oldTableId.outerWidth();
-    // 动态新增表头并复制宽度
-    var newtable=oldTableId.clone(true);
-    newtable.attr("id","newTableId");
-    // 有thead的，表头可以是单行或者多行
-    if(oldTableId.children("thead").length>0){
-        oldTableId.parent().append(newtable.empty().html(newtable.find("thead")));
-        $("#newTableId").find('tr').each(function (i) {
-            $(this).css('height',oldTableId.find('tr').eq(i).height())
-            $(this).find("th").each(function(j){
-                $(this).css('width',oldTableId.find('tr').eq(i).find('th:eq(' + j + ')').innerWidth());
-            })   
-        });
-    }else{
-        // 无thead的，表头默认为第一行
-        oldTableId.parent().append(newtable.empty().html(newtable.find("tr").eq(0)))
-        $("#newTableId").find('th').each(function (i) {
-            $(this).css('width',oldTableId.find('th:eq(' + i + ')').innerWidth());
-        });
-    }  
-    // 新表头的位置，默认为覆盖当前表头，即配合fillHead一起使用
-    $("#newTableId").css({
-        "position": "fixed",
-        "top": h,
-        "left": w,
-        "width": d,
-        // "display":"none",
-        "z-index":2
-    })
-    //窗口大小改变时，对应表头宽度进行自适应
-    $(window).resize(function () {
-        $("#newTableId").width(oldTableId.outerWidth());
-        if(oldTableId.find("thead").length>0){
-            $("#newTableId").find('tr').each(function (i) {
-                $(this).css('height',oldTableId.find('tr').eq(i).height())
-                $(this).find("th").each(function(j){
-                    $(this).css('width',oldTableId.find('tr').eq(i).find('th:eq(' + j + ')').innerWidth());
-                })   
-            });
-        }else{
-            $("#newTableId").find('th').each(function (i) {
-                $(this).css('width',oldTableId.find('th:eq(' + i + ')').innerWidth());
-            });
-        }
-    });
-    console.log(oldTableId.parent().html())
-    // 当表头超出窗口宽度时，出现滚动条，新加的表头需要跟随滚动条移动
-    $(window).scroll(function(){  
-        var w=oldTableId.offset().left;
-        // var dist = oldTableId.offset().top-$(window).scrollTop();
-        // if(dist<0){
-        //     $("#newTableId").show(); 
-        // }else{
-        //     $("#newTableId").hide(); 
-        // }
-        var c = $(this).scrollLeft();
-        $("#newTableId").css({
-            "left": -c+w
+/* 
+    使用示例：
+    <script src="../js/jquery.fixedTableHead.js"></script>
+    <%--固定搜索栏--%>
+    <div id="fixedHead">
+        我是需要被固定的搜索栏
+    </div>
+    <script type="text/javascript">
+        //固定搜索栏与表头
+        $(window).load(function () {
+            $("#fixedHead").fillHead();
+            $("#gvList").fixedTableHead();
         })
-    })
+    </script> 
 
-}
-// 固定搜索栏
-function fillHead(id){
-    var h = $(id).height();
-    var color = $(id).css('background-color');
-    console.log(color);
-    $(id).css({
-        "position":"fixed",
-        "top":0,
-        "width":"100%",
-        "background-color":"#fff",
-        "z-index":"4"
-    });
-    $("body").prepend('<div class="bcg"></div>');
-    $(".bcg").css({
-        "position":"relative",
-        "top":0,
-        "left":0,
-        "height":h,
-        "width":"100%",
-        "z-index":"3"
-    })  
-}
+*/
+;(function($) {
+    jQuery.fn.extend({
+        //固定表头、参数：newheight  新表头距离窗口顶部的距离，为零时在窗口顶部，不设时为原有位置
+        "fixedTableHead":function(newheight){
+            //消除固定表头对日历控件的影响
+            $("#meizzCalendarLayer").css("position", "fixed");
+            $("#newTableId").remove();
+            //$(this)表调用方法的元素
+            var oldTableId = $(this);
+            var scrollTimer;
+            var oldheadertop =0;
+            var oldheaderleft = 0;
+            var oldheaderwidth = 0;
+            if (oldTableId.length > 0) {
+                oldheadertop = oldTableId.offset().top;
+                oldheaderleft = oldTableId.offset().left;
+                oldheaderwidth = oldTableId.outerWidth();
+            }
+            // 动态新增表头并复制宽度
+            var newtable = oldTableId.clone(true);
+            newtable.attr("id", "newTableId");
+            // 有thead
+            if (oldTableId.children("thead").length > 0) {
+                // 添加在原有表格的后面
+                oldTableId.parent().append(newtable.html(newtable.children("thead")));
+                $("#newTableId").width(oldheaderwidth)
+                var newTr = $("#newTableId").find('tr')
+                for (var i = 0; i < newTr.length; i++) {
+                    var element = newTr.eq(i)
+                    element.css('height', oldTableId.find('tr').eq(i).height());
+                    for (var j = 0; j < element.find("th").length; j++) {
+                        var elementTh = element.find("th").eq(j);
+                        elementTh.width(oldTableId.find('tr').eq(i).find('th:eq(' + j + ')').width())
+                    }
+                }
+            } else {
 
-// 表格滚动
-function fixedTableHead2(id){
-    var oldTableId=$(id);
-    var parentId=$(id).parent();
-    var h=oldTableId.offset().top;
-    var w=oldTableId.offset().left;
-    var d = oldTableId.width();
-    // 动态新增表头并复制宽度
-    var newtable=oldTableId.clone(true);
-    newtable.attr("id","newTableId");
-    if(oldTableId.find("thead").length>0){
-        oldTableId.parent().prepend(newtable.remove().html(newtable.find("thead")));
-        $("#newTableId").find('tr').each(function (i) {
-            $(this).css('height',oldTableId.find('tr').eq(i).height())
-            $(this).find("th").each(function(j){
-                $(this).css('width',oldTableId.find('tr').eq(i).find('th:eq(' + j + ')').width());
-            })   
-        });
-    }else{
-        oldTableId.parent().prepend(newtable.remove().html(newtable.find("tr").eq(0)))
-        $("#newTableId").find('th').each(function (i) {
-            $(this).css('width',oldTableId.find('th:eq(' + i + ')').width());
-        });
-    }  
-    // 新表头的位置
-    $("#newTableId").css({
-        "position": "fixed",
-        "top": h,
-        "left": 0,
-        "width": d,
-        "z-index": 999,
-        "display":"none"
-    })
-    //窗口大小改变时，对应表头宽度进行自适应
-    $(window).resize(function () {
-        // alert(1);
-        $("#newTableId").width(oldTableId.width());
-        if(oldTableId.find("thead").length>0){
-            $("#newTableId").find('tr').each(function (i) {
-                $(this).css('height',oldTableId.find('tr').eq(i).height())
-                $(this).find("th").each(function(j){
-                    $(this).css('width',oldTableId.find('tr').eq(i).find('th:eq(' + j + ')').width());
-                })   
+                // 无thead，原理同上
+                oldTableId.parent().append(newtable.html(newtable.find("tr").eq(0)))
+                var element = $("#newTableId").find('tr').eq(0)
+                element.css('height', oldTableId.find('tr').eq(0).height());
+                for (var j = 0; j < element.find("th").length; j++) {
+                    var elementTh = element.find("th").eq(j);
+                    elementTh.width(oldTableId.find('tr').eq(0).find('th:eq(' + j + ')').width())
+                }
+            }
+            // 新表头的位置，默认不显示
+            $("#newTableId").css({
+                "position": "fixed",
+                //"top": oldheadertop  ,
+                "left": oldheaderleft,
+                "width": oldheaderwidth,
+                "display":"none",
+                "z-index":100,   
+            })
+            //确定新表头的高度
+            newheight = ResizeHeight(oldTableId, newheight);
+
+            //窗口大小改变时，对应表头宽度进行自适应
+            $(window).resize(function () {
+                // 重新计算表头宽高与位置
+                handleResize(oldTableId, newheight)
             });
-        }else{
-            $("#newTableId").find('th').each(function (i) {
-                $(this).css('width',oldTableId.find('th:eq(' + i + ')').width());
+
+            // 当表头超出窗口宽度时，出现滚动条，新加的表头需要跟随滚动条移动
+            //这里有个问题，能不能使页面中的表格固定表头，这样的话就要监听表格的父div或者其他有滚动条的父div，感觉很不清晰的样子
+            $(window).scroll(function () {
+                // scroll优化
+                clearTimeout(scrollTimer);
+                scrollTimer = setTimeout(function(){
+                    handleResize(oldTableId, newheight)
+                    //当表头的位置超过新表头显示的位置，新表头出现
+                    //oldTableId.offset().top 老位置 ，newheight 新位置 ，$(window).scrollTop()滚动高度（递增）
+                    var newheight2;
+                    if (newheight == undefined) { newheight2 = oldTableId.offset().top } else { newheight2 = newheight }
+                    var dist = oldTableId.offset().top - newheight2 - $(window).scrollTop();
+                    if (dist < 0) {
+                        $("#newTableId").show();
+                        //setTimeout(resizeTable(oldTableId),1);
+                    } else {
+                        $("#newTableId").hide();
+                    }
+                }, 250);
+
+                // 原表头距离左侧的宽度
+                var w = oldTableId.offset().left;
+                // 横向滚动条滚动的距离
+                var c = $(this).scrollLeft();
+                // 横向滚动处理
+                    $("#newTableId").css({
+                        "left": -c + w
+                    })
+            })
+        },
+        // 固定搜索栏，实现原理：将搜索栏变为fixed，然后添加bcg占位
+        "fillHead": function () {
+            $(".fillHeadbcg").remove();
+            var fillHead = $(this);
+            // 搜索栏样式改变
+            fillHead.css({
+                "position": "fixed",
+                "top": 0,
+                "width": "100%",
+                "background-color": "#fff",
+                "z-index":100
+            });
+            // 搜索栏变为fixed之后bcg替它占位
+            $("body").prepend('<div class="fillHeadbcg"></div>');
+            $(".fillHeadbcg").height(fillHead.innerHeight());
+            $(".fillHeadbcg").css({
+                "position": "relative",
+                "top": 0,
+                "left": 0,
+                "width": "100%",
+                "background-color": "#fff"
+            })
+            // 窗口大小改变时，对应固定栏高度进行自适应
+            $(window).resize(function () {
+                $(".fillHeadbcg").height(fillHead.innerHeight());
             });
         }
     });
-    // 当表头超出表格父窗口宽度时，出现滚动条，新加的表头需要跟随滚动条移动
-    oldTableId.parent().scroll(function () {
-        // console.log(111);
-        if($(this).scrollTop()>0){
-            $("#newTableId").show(); 
-        }else{
-            $("#newTableId").hide(); 
+})(jQuery);
+
+// 处理窗口大小改变
+function handleResize(oldTableId,newheight) {
+    ResizeHeight(oldTableId,newheight);
+    $("#newTableId").width(oldTableId.outerWidth());
+    if (oldTableId.children("thead").length > 0) {
+        var newTr = $("#newTableId").find('tr') 
+        for(var i =0;i<newTr.length;i++){
+            var element = newTr.eq(i)
+            element.css('height', oldTableId.find('tr').eq(i).height());
+            for(var j=0;j<element.find("th").length;j++){
+                var elementTh = element.find("th").eq(j);
+                elementTh.width(oldTableId.find('tr').eq(i).find('th:eq(' + j + ')').width())
+            }
         }
-        var c = $(this).scrollLeft();
-        var w=oldTableId.offset().left;
+    } else {
+        var element = $("#newTableId").find('tr').eq(0)
+        element.css('height', oldTableId.find('tr').eq(0).height());
+        for (var j = 0; j < element.find("th").length; j++) {
+            var elementTh = element.find("th").eq(j);
+            elementTh.width(oldTableId.find('tr').eq(0).find('th:eq(' + j + ')').width())
+        }
+    }
+}
+//新表头高度
+function ResizeHeight(oldTableId,newheight) {
+    if (newheight == undefined) {
         $("#newTableId").css({
-            "left": -c
+            "top": oldTableId.offset().top
         })
-    })
+    } else {
+        $("#newTableId").css({
+            "top": newheight
+        })
+    }
+
+    return newheight;
+
+
 }
+
